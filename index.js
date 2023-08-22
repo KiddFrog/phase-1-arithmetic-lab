5a9eb02997c0112f1d8708ab4e036514b0fda21c
domm <dommferger@gmail.com>
2023-08-22 10:14:39 -0400

    gotta speed up

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const num1 = 31;
+const num2 = 2;
+function multi(){
+let multiplied = num1 * num2;
+return multiplied;
 
-// Write your code here
+}
+let multiply = multi();
+/// next part
+// generate rand intg
+let random = Math.floor(Math.random() * 10) + 1;
+//console.log(random);
+
+// mod 1) is an equation that calculates a remainder that is equal to 4
+
+const num3 = 6;
+const num4 = 2;
+function sub(){
+    let subtract = num3 - num4;
+    return subtract;
+}
+let mod = sub();
+///////////// 20 highest
+
+
+function maxSort(){
+    let sorting =  Math.max(1, 2, 3, 4, 20);
+    return sorting;
+}
+
+let max = maxSort();
